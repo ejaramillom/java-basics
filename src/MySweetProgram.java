db210ae8106928791057e0726d5c0cb76554523e
Emmanuel <ejaramilo@hotmail.com>
2022-11-22 14:30:35 -0500

    0011 understanding java foundations

--- src/MySweetProgram.java
+++ src/MySweetProgram.java
@@ -1,7 +1,29 @@
 
 public class MySweetProgram {
-  public static void main(String[] args) {
-	// something to do in java
-	System.out.print("Hello there!");
-  }
+	// MySweetProgram is an identifier
+
+	// this is a method
+	public static void main(String[] args) {
+		// something to do in java
+		System.out.println("Hello there!");
+		// system.out is an statement
+		
+		var x = new	MySweetProgram(); // this creates an object
+		x.tacos(); // this works because of the static keyword
+	}
+
+	public void tacos() {
+		// something to do in java
+		System.out.println("Hello there in tacos!");
+		// system.out is an statement
+	}
+	// the content are members
+
+	String x = new String("Hello"); // this is an example of a property
+
+	// syntax of java usually consist of identifiers made up of properties and
+	// statements
 }
+
+// curly braces are used to show what is part of a class identifier
+// it is usually managed in OOP
