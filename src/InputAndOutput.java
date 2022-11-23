d9dbd182114cfb3d11c9a053b153e6f85e443e05
Emmanuel <ejaramilo@hotmail.com>
2022-11-23 11:57:11 -0500

    0049 primitives and objects

--- src/InputAndOutput.java
+++ src/InputAndOutput.java
@@ -10,6 +10,11 @@ public class InputAndOutput {
 		Scanner scanner = new Scanner(System.in); // we use a scanner to ask for an argument
 		String name = scanner.nextLine(); // then in the next line in console we receive the input
 
+		int x = 5; // primitive evaluation
+		Integer y = 5; // object evaluation (5 is autoboxed)
+		
+		System.out.println(x);
+		System.out.println(y);
 		System.out.println("hello " + name);
 
 	}
@@ -21,3 +26,12 @@ public class InputAndOutput {
 // operator - work on operands to produce a value
 // operand - objects the operator works on
 // literals - the value
+
+// data types - how the computer interprets the data
+// java is a statically typed language, so we have to declare the variables type
+
+// primitive types - char, boolean, byte, short, int, long, float, double
+// objects - instance of a class
+
+// statically typed - variables are given data types up front
+// dynamically typed language - variables do not have a fixed type
